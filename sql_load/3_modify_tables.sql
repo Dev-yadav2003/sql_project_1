f4bf62736d86389686c4165f24bf2a61b12ef612
Dev-yadav2003 <devyadav.lfc.dtu@gmail.com>
2025-07-08 16:57:10 +0530

    setting up repo

---
+++ sql_load/3_modify_tables.sql
@@ -0,0 +1,21 @@
+
+COPY company_dim
+FROM 'D:\Sql_project_job_posting\csv_files\company_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY skills_dim
+FROM 'D:\Sql_project_job_posting\csv_files\skills_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY job_postings_fact
+FROM 'D:\Sql_project_job_posting\csv_files\job_postings_fact.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY skills_job_dim
+FROM 'D:\Sql_project_job_posting\csv_files\skills_job_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+
+
+
+
